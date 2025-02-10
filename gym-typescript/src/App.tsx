9e751ab60a2f997289c1eeea3d540ab2bb8fcfc3
PICKELS\fisty <szakacs.barna@gosocialdev.eu>
2025-02-10 18:52:57 +0200

    first commit

---
+++ gym-typescript/src/App.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+function App() {
+  
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
+  return (
+    <>
+    <div className='app'>
+ <Navbar
+ selectedPage = {selectedPage}
+ setSelectedPage = {setSelectedPage}
+ />
+    </div>
+     
+     </>
+  )
+}
+
+export default App
